82d5ce1f0a31bfe055d189da5e1993234ec67d4a
PavlovPav <lokimean@mail.ru>
2018-12-26 19:39:21 +0300

    telega bot

--- homework05/schedule.py
+++ homework05/schedule.py
@@ -1,11 +1,13 @@
+import telebot
 import requests
 import config
-import telebot
 from bs4 import BeautifulSoup
 
 
-bot = telebot.TeleBot(config.access_token)
-
+f = open(".teletoken", "r")
+token = f.read().strip()
+bot = telebot.TeleBot(token)
+config.domain = "http://www.ifmo.ru/ru/schedule/0"
 
 def get_page(group, week=''):
     if week:
@@ -19,7 +21,7 @@ def get_page(group, week=''):
     return web_page
 
 
-def parse_schedule_for_a_monday(web_page):
+def get_schedule(web_page):
     soup = BeautifulSoup(web_page, "html5lib")
 
     # Получаем таблицу с расписанием на понедельник
@@ -37,49 +39,25 @@ def parse_schedule_for_a_monday(web_page):
     lessons_list = schedule_table.find_all("td", attrs={"class": "lesson"})
     lessons_list = [lesson.text.split('\n\n') for lesson in lessons_list]
     lessons_list = [', '.join([info for info in lesson_info if info]) for lesson_info in lessons_list]
-
     return times_list, locations_list, lessons_list
 
 
 @bot.message_handler(commands=['monday'])
 def get_monday(message):
-    """ Получить расписание на понедельник """
     _, group = message.text.split()
     web_page = get_page(group)
-    times_lst, locations_lst, lessons_lst = \
-        parse_schedule_for_a_monday(web_page)
+    times_lst, locations_lst, lessons_lst = get_schedule(web_page)
+
     resp = ''
     for time, location, lession in zip(times_lst, locations_lst, lessons_lst):
         resp += '<b>{}</b>, {}, {}\n'.format(time, location, lession)
+
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
-def get_schedule(message):
-    """ Получить расписание на указанный день """
-    # PUT YOUR CODE HERE
-    pass
-
-
-@bot.message_handler(commands=['near'])
-def get_near_lesson(message):
-    """ Получить ближайшее занятие """
-    # PUT YOUR CODE HERE
-    pass
-
-
-@bot.message_handler(commands=['tommorow'])
-def get_tommorow(message):
-    """ Получить расписание на следующий день """
-    # PUT YOUR CODE HERE
-    pass
-
-
-@bot.message_handler(commands=['all'])
-def get_all_schedule(message):
-    """ Получить расписание на всю неделю для указанной группы """
-    # PUT YOUR CODE HERE
-    pass
+# @bot.message_handler(content_types=['text'])
+# def echo(message):
+#     bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == '__main__':
